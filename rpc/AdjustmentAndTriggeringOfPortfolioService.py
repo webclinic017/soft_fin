44de15bd490b4fc2a2eaf0b81da9d93bba1d7d73
steve <chph13420146901@gmail.com>
2019-09-12 23:51:29 +0800

    feat: add some print expression for debugging

--- rpc/AdjustmentAndTriggeringOfPortfolioService.py
+++ rpc/AdjustmentAndTriggeringOfPortfolioService.py
@@ -5,13 +5,16 @@ import api
 
 class AdjustmentAndTriggeringOfPortfolioService(AdjustmentAndTriggeringOfPortfolioServicer):
     def PortFolioVar(self, request, context):
+        print("request: ",request)
         res = api.portfolio_var(request.portfolio, request.setting)
         return adjustment_and_triggering_of_portfolio_pb2.PortFolioOutput(value=res)
 
     def PortfolioVolatility(self, request, context):
+        print("request: ",request)
         res = api.portfolio_volatility(request.portfolio, request.cash, request.setting)
         return adjustment_and_triggering_of_portfolio_pb2.PortFolioOutput(value=res)
 
     def PortfolioDiff(self, request, context):
+        print("request: ",request)
         res = api.portfolio_diff(request.portfolio_id, request.portfolio, request.cash, request.alpha)
         return adjustment_and_triggering_of_portfolio_pb2.PortFolioOutput(value=res)
--- rpc/RegularAdjustmentService.py
+++ rpc/RegularAdjustmentService.py
@@ -6,17 +6,21 @@ import api
 class RegularAdjustmentService(regular_adjustment_pb2_grpc.RegularAdjustmentServicer):
 
     def StockLeastPosition(self, request, context):
+        print("request: ", request)
         res = api.stock_least_position(request.user_position, request.setting)
         return regular_adjustment_pb2.StockLeastPositionOutput(value=res)
 
     def StockVolatility(self, request, context):
+        print("request: ", request)
         res = api.stock_volatility(request.stock_code, request.time, request.setting)
         return regular_adjustment_pb2.StockVolatilityOutput(value=res)
 
     def StockChange(self, request, context):
+        print("request: ", request)
         res = api.stock_change(request.stock_code, request.time, request.top, request.bottom)
         return regular_adjustment_pb2.StockChangeOutput(value=res)
 
     def StockMeanReturn(self, request, context):
+        print("request: ", request)
         res = api.stock_mean_return(request.stock_code, request.time, request.setting)
         return regular_adjustment_pb2.StockMeanReturnOutput(value=res)
