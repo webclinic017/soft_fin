c62843b5b2e44020424dd97435a302e510407c97
qijiale76 <171850589@smail.nju.edu.cn>
2019-09-12 22:36:18 +0800

    u

--- RpcServer.py
+++ RpcServer.py
@@ -11,10 +11,12 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    regular_adjustment_pb2_grpc.add_RegularAdjustmentServicer_to_server(RegularAdjustmentService(), server)
-    conditional_trigger_pb2_grpc.add_ConditionalTriggerServicer_to_server(ConditionalTriggerService(), server)
+    regular_adjustment_pb2_grpc.add_RegularAdjustmentServicer_to_server(
+        RegularAdjustmentService.RegularAdjustmentService(), server)
+    conditional_trigger_pb2_grpc.add_ConditionalTriggerServicer_to_server(
+        ConditionalTriggerService.ConditionalTriggerService(), server)
     adjustment_and_triggering_of_portfolio_pb2_grpc.add_AdjustmentAndTriggeringOfPortfolioServicer_to_server(
-        AdjustmentAndTriggeringOfPortfolioService(), server)
+        AdjustmentAndTriggeringOfPortfolioService.AdjustmentAndTriggeringOfPortfolioService(), server)
 
     server.add_insecure_port('[::]:50051')
     server.start()
